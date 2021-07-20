5df4599c5085e75fd74fe95087d2ba5338953779
Ramakrishna BK <ramakrishna-k.b@microfocus.com>
2021-07-20 17:44:18 +0530

    100th problem

---
+++ climbing-stairs.h
@@ -0,0 +1,71 @@
+// You are climbing a staircase. It takes n steps to reach the top.
+
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+// Example 1:
+
+// Input: n = 2
+// Output: 2
+// Explanation: There are two ways to climb to the top.
+// 1. 1 step + 1 step
+// 2. 2 steps
+// Example 2:
+
+// Input: n = 3
+// Output: 3
+// Explanation: There are three ways to climb to the top.
+// 1. 1 step + 1 step + 1 step
+// 2. 1 step + 2 steps
+// 3. 2 steps + 1 step
+
+// Constraints:
+
+// 1 <= n <= 45
+
+// https://leetcode.com/problems/climbing-stairs/
+
+// Runtime: 0 ms, faster than 100.00% of C++ online submissions for Climbing Stairs.
+// Memory Usage: 5.9 MB, less than 53.16% of C++ online submissions for Climbing Stairs.
+
+class Solution
+{
+public:
+    int climbStairs(int n)
+    {
+        if (n == 1)
+        {
+            return 1;
+        }
+
+        // The problem is nothing but a fibonacci number
+        auto f = 1, s = 2;
+        for (auto i = 3; i <= n; i++)
+        {
+            auto t = f + s;
+            f = s;
+            s = t;
+        }
+        return s;
+    }
+};
+
+// Dynamic programming based solution
+// Runtime: 0 ms, faster than 100.00% of C++ online submissions for Climbing Stairs.
+// Memory Usage: 6.2 MB, less than 12.74% of C++ online submissions for Climbing Stairs.
+class Solution
+{
+public:
+    int climbStairs(int n)
+    {
+        // Using dynamic programming
+        vector<int> dp(n + 2);
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (auto i = 3; i <= n; i++)
+        {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+};
